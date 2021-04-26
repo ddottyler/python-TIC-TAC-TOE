e6bafa0db708ca885902dd701305b141e99b530b
Dan T <dp.tyler@hotmail.co.uk>
2021-04-26 11:13:41 +0100

    logic added to check_rows function

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def play_game():
 
     if winner == "X" or winner == "O":
         print(winner + " won.")
-    elsif winner == None:
+    elif winner == None:
         print("Tie.")
 
 
@@ -57,14 +57,48 @@ def handle_turn(player):
 
 
 def check_if_game_over():
-    check_if_win()
+    check_for_winner()
     check_if_tie()
 
 
-def check_if_win():
-    # check rows
-    # check columns
-    # check diagonals
+def check_for_winner():
+
+    global winner
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+    if row_winner:
+        winner = row_winner()
+    elif column_winner:
+        winner = column_winner()
+    elif diagonal_winner:
+        winner = diagonal_winner()
+    else:
+        winner = None
+    return
+
+
+def check_rows():
+    global game_still_going
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    if row_1:
+        return board[0]
+    elif row_2:
+        return board[3]
+    elif row_3:
+        return board[6]
+    return
+
+
+def check_columns():
+    return
+
+
+def check_diagonals():
     return
 
 
