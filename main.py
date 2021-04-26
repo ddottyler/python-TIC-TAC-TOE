7ddae17d78fa43829c3ee587e9117a070c32fac0
Dan T <dp.tyler@hotmail.co.uk>
2021-04-26 10:55:34 +0100

    core logic added but game now in a never ending loop

--- main.py
+++ main.py
@@ -9,9 +9,21 @@
 # check tie
 # flip player
 
+# ------ Global Variables ------
+
+# Game board
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
+
+#  If game is still going
+game_still_going = True
+
+#  Who won? or tie?
+winner = None
+
+# Whos turn is it
+current_player = "X"
 
 
 def display_board():
@@ -22,15 +34,46 @@ def display_board():
 
 def play_game():
     display_board()
-    handle_turn()
+
+    while game_still_going:
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
+
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elsif winner == None:
+        print("Tie.")
 
 
-def handle_turn():
+def handle_turn(player):
     position = input("Choose a position from 1-9: ")
     position = int(position) - 1
 
     board[position] = "X"
     display_board()
+
+
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+
+def check_if_win():
+    # check rows
+    # check columns
+    # check diagonals
+    return
+
+
+def check_if_tie():
+    return
+
+
+def flip_player():
+    return
 
 
 play_game()
